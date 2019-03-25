c0d1eb3bc797e43bc2a15db236093e82152397f3
BorangaziyevA <Alisher@DESKTOP-TO8LLFK>
2019-03-25 13:53:23 +0600

    Добавьте файлы проекта.

---
+++ 05.03.2019HW/Source.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<Windows.h>
+
+using namespace std;
+
+////2//2//////2////2////2/
+int main()
+{
+	srand(time(NULL));
+
+	int vos;
+	int var;
+
+	cin >> var;
+	int x = 10000;
+	int  y = 0;
+
+	for (int vosm = 512; vosm >= 1; vosm /= 8)
+	{
+		vos = var / vosm;
+		var %= vosm;
+		y = y + (x*vos);
+		x = x / 10;
+	}
+
+	if (y > 9999)
+	{
+		y = y / 10 ;
+	}
+
+	cout << endl << y << endl;
+
+	system("pause");
+	return 0;
+}
